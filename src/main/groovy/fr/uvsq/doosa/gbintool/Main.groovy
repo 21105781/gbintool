d84e8b9e084d89a26e531527652776980a5e5825
Estelle <echariat@hotmail.fr>
2015-10-04 23:57:00 +0200

    ajout test junit

--- src/main/groovy/fr/uvsq/doosa/gbintool/Main.groovy
+++ src/main/groovy/fr/uvsq/doosa/gbintool/Main.groovy
@@ -1,14 +1,23 @@
 package fr.uvsq.doosa.gbintool
 
+import org.junit.Test
+
+class MainTest {
+ @Test
+ void shouldReturnHello() {
+ Main m = new Main()
+ assert 'Hello' == m.returnHello()
+ }
+}
+package fr.uvsq.doosa.gbintool
 import groovy.util.logging.Slf4j
 
 @Slf4j
 class Main {
-    static void main(def args) {
-        log.info 'Hello'
-    }
-
-    String returnHello() {
-        'Hello'
-    }
+ static void main(def args) {
+  log.info 'Hello'
+ }
+String returnHello() {
+ 'Hello'
+ }
 }
